52fb19963ce061e3c67fe9633ec8ff8a1f766bdb
Seko461 <sekopierre@gmail.com>
2024-05-07 15:36:31 +0200

    step 1 done

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.app')
+
+@section('content')
+<div class="container">
+    <h2>Home page</h2>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus exercitationem rem, consectetur officiis porro, impedit dicta dolores repellat et sequi labore molestias dolorem obcaecati magni odio, ad amet sapiente eligendi!
+    </p>
+</div>
+@endsection
