7e5fa151a95aa58a765bf6053e9287acc2bbfd2e
Seko461 <sekopierre@gmail.com>
2024-05-07 17:49:31 +0200

    add style to main

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,14 +3,17 @@
 @section('content')
 <section class="py-5">
     <div class="container">
-        <h2>Home Page</h2>
+        <span class="current text-uppercase fs-2">Current series</span>
         <div class="row row-cols-1 row-cols-sm-3 row-cols-md-6 g-4">
             @forelse ($comics as $comic )
             <div class="col">
-                <div class="card h-100">
-                    <div class="card-body">
-                        {{$comic['title']}}
-                    </div>
+                <div class="card h-100 rounded-0 border-0 bg-transparent text-white" >
+                    
+                        <img height="200"src="{{$comic['thumb']}}" alt="">
+                
+                        <div class="titolo px-1 ">
+                            {{$comic['title']}}
+                        </div>
                 </div>
             </div>
             @empty
