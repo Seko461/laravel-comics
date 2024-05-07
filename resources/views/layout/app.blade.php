8eb7766c5d9ad5272033d386e9864f36aa926902
Seko461 <sekopierre@gmail.com>
2024-05-07 16:45:55 +0200

    add nav structure

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,11 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
+    <title>Laravel @yield('title-page','DC')</title>
+    <!-- Fontawesome CDN -->
+    <script src="https://kit.fontawesome.com/f54afad4a3.js" crossorigin="anonymous"></script>
     <!-- Styles -->
     @vite('resources/js/app.js')
 
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,35 @@
 
 <header>
-    Header
+    <nav class="nav d-flex justify-content-center align-items-center py-2">
+        <img  src="{{ Vite::asset('resources/assets/images/dc-logo.png') }}" alt="">
+        <div class="links text-uppercase text-decoration-none gap-4">
+            <a class="text-decoration-none mx-2"href="#">CHARACTERS</a>
+            <a class="text-decoration-none mx-2"href="#">COMICS</a>
+            <a class="text-decoration-none mx-2"href="#">MOVIES</a>
+            <a class="text-decoration-none mx-2"href="#">TV</a>
+            <a class="text-decoration-none mx-2"href="#">GAMES</a>
+            <a class="text-decoration-none mx-2"href="#">COLLECTIBLES</a>
+            <a class="text-decoration-none mx-2"href="#">VIDEOS</a>
+            <a class="text-decoration-none mx-2"href="#">FANS</a>
+            <a class="text-decoration-none mx-2"href="#">NEWS</a>
+            
+        </div>
+        <div class="nav-item dropdown text-uppercase">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                   Shop
+                </a>
+                 <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+        </div>
+        <form class="d-flex" role="search">
+            <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn" type="submit">
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </button>
+      </form>
+
+    </nav>
 </header>
